d7c85d3e9d3c40ab3fa0b84e19b4d3742c243734
adivinpatel <105838977+adivinpatel@users.noreply.github.com>
2023-05-16 22:28:16 -0700

    Delete generatedataframe.py

--- generatedataframe.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-#this dataset is used to generate the hindi actors movie dataframe
-castofhindimovies = pd.read_csv(r'data\castofhindimovies.csv')
-hindiactorsmovies = castofhindimovies.groupby('Actor')['Movie'].agg(list).reset_index()
-#movies in list by actors
-hindiactorsmovies.columns = ['Actor', 'Movies']
-#redefined to put actors in lists by movie
-castofhindimovies = castofhindimovies.groupby('Movie')['Actor'].agg(list).reset_index()
